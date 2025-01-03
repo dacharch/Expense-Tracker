9ab54747e940811d1e7ce6376229658839e70bab
Neeraj Kumar <nk35715945@gmail.com>
2025-01-03 18:01:13 +0530

    Project template has been done

---
+++ app/(tabs)/Analysis.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+const Analysis = () => {
+  return (
+    <View>
+          <Text> Analysis</Text>
+    </View>
+  )
+}
+
+export default Analysis
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,49 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    screenOptions={{
+        tabBarActiveTintColor: '#ffd33d',
+        headerShown:false,
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+        backgroundColor: '#25292e',
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
+        
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'receipt-sharp' : 'receipt-outline'} color={color} size={24} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="Analysis"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Analysis',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'analytics-sharp' : 'analytics-sharp'} color={color} size={24}/>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Reports"
+        options={{
+          title: 'Reports',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'book-sharp' : 'book-outline'} color={color} size={24}/>
+          ),
         }}
       />
     </Tabs>
