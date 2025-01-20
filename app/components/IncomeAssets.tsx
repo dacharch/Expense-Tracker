29802ef8fb50aac56f36700b42325fce9336ac4c
Neeraj Kumar <nk35715945@gmail.com>
2025-01-20 22:54:10 +0530

    Update the Whole component

--- app/components/IncomeAssets.tsx
+++ app/components/IncomeAssets.tsx
@@ -1,13 +1,52 @@
 import React from 'react'
-import { View,Text } from 'react-native'
+import { View,Text,StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome6';
+
 
 const IncomeAssets = () => {
   return (
-     <View>
-         <Text>Income Assets</Text>
+     <View style={styles.container}>
+
+      <View style={styles.icon_container}>
+      <View style={styles.icon} >
+         <Icon
+             name='wallet'
+             size={40}
+          />
+      </View>
+      <View style={styles.icon}>
+           <Icon
+              name="business-time"
+              size={40}
+           />
+      </View>
+      </View>
      </View>
   )
 }
+
+const styles = StyleSheet.create({
+   container:{
+       padding:20,
+       borderColor:'black',
+  
+   },
+
+   icon_container:{
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-between'
+   },
+
+    icon:{
+       backgroundColor:'gray',
+       borderRadius:'50%',
+       width:65,
+       height:65,
+       alignItems:'center',
+       padding:10,
+    }
+})
 
 export default IncomeAssets
 
